2fb4a9b6e383c97aa67a5bb878ff1df2c30c1324
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-11-14 23:48:57 -0600

    Clean code.

--- processServer/server.go
+++ processServer/server.go
@@ -26,7 +26,6 @@ func f(myProcess process.Process) {
 		max uint64
 	)
 	max = 18446744073709551615
-	// fmt.Println(myProcess)
 	for i = myProcess.Value; i < max; i++ {
 		select {
 		case msg1 := <-myIdChannel:
@@ -39,7 +38,7 @@ func f(myProcess process.Process) {
 			}
 		default:
 			if mostrarProcesos {
-				fmt.Println("id: ", myProcess.Id, ":", i)
+				fmt.Println("id", myProcess.Id, ":", i)
 			}
 		}
 		time.Sleep(time.Millisecond * 500)
@@ -49,7 +48,6 @@ func f(myProcess process.Process) {
 func isClientConnected(id uuid.UUID) bool {
 	isConnected := false
 	for e := myClientsList.Front(); e != nil; e = e.Next() {
-		// do something with e.Value
 		if e.Value == id {
 			isConnected = true
 		}
@@ -69,7 +67,6 @@ func server() {
 				fmt.Println(err)
 				continue
 			} else {
-				// Create a new list and put some numbers in it.
 				var clientId uuid.UUID
 				err = gob.NewDecoder(c).Decode(&clientId)
 				fmt.Println("Received a client id:", clientId)
@@ -78,22 +75,7 @@ func server() {
 					continue
 				}
 				if isClientConnected(clientId) {
-					// fmt.Println("This client id is already known:", clientId)
-					// var aux process.Process
-					// err := gob.NewDecoder(c).Decode(&aux)
-					// fmt.Println(aux)
-					// if err != nil {
-					// 	fmt.Println("Error**\n", err)
-					// 	continue
-					// } else {
-					// 	fmt.Println("Mensaje recibido:", aux)
-					// 	myProcessesList.PushBack(aux)
-					// 	go f(aux)
-
-					// 	// c.Close()
-					// 	fmt.Println("Disconnected...")
-					// }
-					retrieveProcess2(c, clientId)
+					retrieveProcess(c, clientId)
 				} else if myClientsList.Len() < MAXCONN {
 					fmt.Println("This client id", clientId, "is not registered...")
 					myClientsList.PushBack(clientId)
@@ -101,57 +83,25 @@ func server() {
 					fmt.Println("Finish handling client")
 				}
 			}
-			// c.Close()
 		}
 	}
 }
 
 func handleClient(c net.Conn) {
-	//err := gob.NewDecoder(c).Decode(&proceso)
 	if myProcessesList.Len() > 0 {
 		var proceso = myProcessesList.Front().Value.(process.Process) //proceso["channel"] <- proceso["id"]
 		myIdChannel <- proceso.Id
 		reply := <-myReturnChannel
-		// fmt.Println("Respuesta: ", reply)
-		// if reply > 0 {
-		// 	proceso.Valor = reply
-		// 	fmt.Println("Proceso: ", proceso)
-		// 	err := gob.NewEncoder(c).Encode(proceso)
-		// 	if err != nil {
-		// 		fmt.Println(err)
-		// 	} else {
-		// 		fmt.Println("Mensaje:", proceso)
-		// 	}
-		// 	myProcessesList.Remove(myProcessesList.Front())
-		// }
-		nuevoProceso := reply
-		fmt.Println("Proceso: ", proceso)
-		fmt.Println("Proceso nuevo: ", nuevoProceso)
-		err := gob.NewEncoder(c).Encode(nuevoProceso)
+		thisProcess := reply
+		fmt.Println("Proceso a enviar: ", thisProcess)
+		err := gob.NewEncoder(c).Encode(thisProcess)
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println("Mensaje:", nuevoProceso)
+			fmt.Println("Mensaje:", thisProcess)
 		}
 		myProcessesList.Remove(myProcessesList.Front())
 	}
-}
-
-func retrieveProcess(c net.Conn) {
-	var aux process.Process
-	err := gob.NewDecoder(c).Decode(&aux)
-	fmt.Println(aux)
-	if err != nil {
-		fmt.Println("Error**\n", err)
-		return
-	}
-
-	fmt.Println("Mensaje recibido:", aux)
-	myProcessesList.PushBack(aux)
-	go f(aux)
-
-	c.Close()
-	fmt.Println("Disconnected...")
 }
 
 func deleteClient(clientId uuid.UUID) bool {
@@ -161,17 +111,16 @@ func deleteClient(clientId uuid.UUID) bool {
 		if e.Value == clientId {
 			myClientsList.Remove(e)
 			wasDeleted = true
-			fmt.Println("Was deleted:", wasDeleted)
+			fmt.Println("Client was deleted:", wasDeleted)
 		}
 	}
 	return wasDeleted
 }
 
-func retrieveProcess2(c net.Conn, clientId uuid.UUID) {
+func retrieveProcess(c net.Conn, clientId uuid.UUID) {
 	fmt.Println("This client id is already known:", clientId)
 	var aux process.Process
 	err := gob.NewDecoder(c).Decode(&aux)
-	fmt.Println(aux)
 	if err != nil {
 		fmt.Println("Error**\n", err)
 	} else {
@@ -180,23 +129,8 @@ func retrieveProcess2(c net.Conn, clientId uuid.UUID) {
 		fmt.Println("Len Processes:", myProcessesList.Len())
 		deleteClient(clientId)
 		go f(aux)
-
-		// c.Close()
 		fmt.Println("Disconnected...")
 	}
-	// err = gob.NewDecoder(c).Decode(&aux)
-	// fmt.Println(aux)
-	// if err != nil {
-	// 	fmt.Println("Error**\n", err)
-	// 	return
-	// } else {
-	// 	fmt.Println("Mensaje recibido:", aux)
-	// 	myProcessesList.PushBack(aux)
-	// 	go f(aux)
-
-	// 	// c.Close()
-	// 	fmt.Println("Disconnected...")
-	// }
 }
 
 func main() {
